5f18ac32620a8d415d6cb0d4a46eaed30d12c295
sergious234 <sergious234@gmail.com>
2022-06-29 13:12:55 +0200

    Optimiced C

---
+++ libhypoc/hypoc.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+#include <complex.h> // complex, cabs
+
+inline extern double good_cabs(double z){
+    return cabs(z);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use num::Complex;
-use std::{fs::File, io::Write};
+use num_complex::Complex;
+use std::{fs::File, io::Write, time::Instant};
 
 const WIDTH: usize = 2_000;
 const HEIGHT: usize = 2_000;
@@ -8,6 +8,10 @@ type Pixels = Vec<u8>;
 const F_HEIGHT: f64 = HEIGHT as f64;
 const  F_WIDTH: f64 = WIDTH as f64;
 
+#[link(name="hypoc", kind="static")]
+extern "C" {
+    fn good_cabs(v1: f64)->f64;
+}
 
 fn write_image(pixels: &mut Pixels, path: &str){
     let header: &str = &format!("P3\n{} {}\n{}\n", WIDTH, HEIGHT, 255);
@@ -18,24 +22,23 @@ fn write_image(pixels: &mut Pixels, path: &str){
     for p in 0..WIDTH*HEIGHT{
         let c = pixels[p];
         aux = aux + &format!("{} {} {}\n", c, c, c);
-        if p % 1000 == 0 {
+        if p % 10000 == 0 {
             image.write_all(aux.as_bytes()).unwrap();
             aux = "".to_owned();
-            println!("{:.5}%", ((p as f64/ (F_HEIGHT*F_HEIGHT)) * 100.0));
+            //println!("{:.5}%", ((p as f64/ (F_HEIGHT*F_HEIGHT)) * 100.0));
         }
     }
     
 }
 
 fn write_pixel(x: usize, y: usize, color: u8, pixels: &mut Pixels){
-
     let index;
 
     if y>HEIGHT || x>WIDTH {
         panic!("Write pixel out of range");
     }
     index = (y*WIDTH) + x;
-    (*pixels)[index] = color;
+    pixels[index] = color;
 }
 
 
@@ -51,36 +54,52 @@ fn clear_image(pixels: &mut Pixels){
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+fn unroll(y: usize, im: f64, i: &Complex<f64>, pixels: &mut Pixels){
 
-fn mandelbrot(pixels: &mut Pixels){    
+    for x in 0..WIDTH{
+        let re = ((x as f64 - (F_WIDTH / 2.0)) / F_WIDTH*2.5) - 0.5;
+        let mut z = re + im * i;
+        let c = z;
+
+        for i in 0..255u8{
+            z = (z.powi(2)) + c;
+
+            if unsafe{good_cabs(z.im + re)} > 2.5{
+                write_pixel(x, y, i, pixels);
+            }
+
+            // if z.norm() > 2.5 {
+            //     write_pixel(x, y, i, pixels);
+            // }
+        }
+    }
+}
+
+fn mandelbrot(pixels: &mut Pixels){ 
+    let _if = HEIGHT / 20;   
+    let i = Complex::i();
     for y in 0..HEIGHT{
         
         let im: f64 = (y as f64  - (F_HEIGHT/2.0)) / F_HEIGHT * 2.5;
-        
-        for x in 0..WIDTH{
-            let re = ((x as f64 - (F_WIDTH/2.0)) / F_WIDTH*2.5) - 0.5;
-            let mut z = re + im * Complex::i();
-            let c = z;
-
-            for i in 0..255u8{
-                z = (z*z) + c;
-                if z.norm() > 2.5 {
-                    write_pixel(x, y, i, pixels);
-                }
-            }
+        unroll(y, im, &i, pixels);
+        if y%_if == 0{
+            println!("{:.2}%", ((y as f64/ F_WIDTH) * 100.0));
         }
-        println!("{:.5}%", ((y as f64/ F_WIDTH) * 100.0));
     }
 }
 
 fn main() {
     let file_name = "MaldelbrotR.ppm".to_owned();
     let mut pixels: Vec<u8> = Vec::with_capacity(WIDTH*HEIGHT);
-    //let mut pixels = Box::new([0 as u8; WIDTH*HEIGHT]);
 
     clear_image(&mut pixels);
-    mandelbrot(&mut pixels);
-    write_image(&mut pixels, &file_name);
 
-    println!("Hello, world!");
+    let now = Instant::now();
+    mandelbrot(&mut pixels);
+    let end = Instant::now();
+
+    write_image(&mut pixels, &file_name);
+    println!("{}", end.duration_since(now).as_secs_f64());
+
 }
